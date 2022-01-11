b97ccc7d69d6a1f4fb503791f0c8efab910847ce
Mehdi Yaqoubi <mehdi.code@pm.me>
2022-01-11 13:57:25 +0330

    Apartment, House, Store class added

--- estate.py
+++ estate.py
@@ -18,3 +18,30 @@ class EstateAbstract(BaseClass):
     @abstractmethod
     def show_description(self):
         pass
+
+
+class Apartment(EstateAbstract):
+    def __init__(self, has_elevator, has_parking, floor, *args, **kwargs):
+        self.has_elevator = has_elevator
+        self.has_parking = has_parking
+        self.floor = floor
+        super().__init__(*args, **kwargs)
+
+    def show_description(self):
+        print(f"Apartment: {self.id}")
+
+
+class House(EstateAbstract):
+    def __init__(self, has_yard, floor_count, *args, **kwargs):
+        self.has_yard = has_yard
+        self.floor_count = floor_count
+        super().__init__(*args, **kwargs)
+
+    def show_description(self):
+        print(f"House: {self.id}")
+
+
+class Store(EstateAbstract):
+
+    def show_description(self):
+        print(f"Store: {self.id}")
