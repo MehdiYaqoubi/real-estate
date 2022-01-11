2ee0bccc97c796594fe9c8342a4bf02906e9a328
Mehdi Yaqoubi <mehdi.code@pm.me>
2022-01-11 13:48:19 +0330

    EstateAbstract class added

---
+++ estate.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod
+from base import BaseClass
+
+
+class EstateAbstract(BaseClass):
+    def __init__(
+            self, user, area, room_count, built_year, region, address, *args,
+            **kwargs
+    ):
+        self.user = user
+        self.area = area
+        self.room_count = room_count
+        self.built_year = built_year
+        self.region = region
+        self.address = address
+        super().__init__(*args, **kwargs)
+
+    @abstractmethod
+    def show_description(self):
+        pass
