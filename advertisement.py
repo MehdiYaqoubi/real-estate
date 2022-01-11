ce24949e0cbaa949053444e12d6f6cbbf4fdfa96
Mehdi Yaqoubi <mehdi.code@pm.me>
2022-01-11 17:07:32 +0330

    ApartmentSell, ApartmentRent, HouseSell, HouseRent, StoreSell, StoreRent was added

---
+++ advertisement.py
@@ -0,0 +1,38 @@
+from estate import Apartment, House, Store
+from deal import Sell, Rent
+
+
+class ApartmentSell(Apartment, Sell):
+    def show_details(self):
+        self.show_description()
+        self.show_price()
+
+
+class ApartmentRent(Apartment, Rent):
+    def show_details(self):
+        self.show_description()
+        self.show_price()
+
+
+class HouseSell(House, Sell):
+    def show_details(self):
+        self.show_description()
+        self.show_price()
+
+
+class HouseRent(House, Rent):
+    def show_details(self):
+        self.show_description()
+        self.show_price()
+
+
+class StoreSell(Store, Sell):
+    def show_details(self):
+        self.show_description()
+        self.show_price()
+
+
+class StoreRent(Store, Rent):
+    def show_details(self):
+        self.show_description()
+        self.show_price()
